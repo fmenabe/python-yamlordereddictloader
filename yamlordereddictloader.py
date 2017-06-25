73593db021656ba7ed8b6243e243edae2a0b2832
François Ménabé <francois.menabe@gmail.com>
2017-06-25 14:58:04 +0200

    Add a safe loader.

--- yamlordereddictloader.py
+++ yamlordereddictloader.py
@@ -1,54 +1,75 @@
 # -*- coding: utf-8 -*-
-"""A YAML loader that loads mappings into ordered dictionaries.
-(http://stackoverflow.com/questions/5121931/in-python-how-can-you-load-yaml-mappings-as-ordereddicts)
-"""
 
-import yaml
+"""YAML loaders and dumpers for PyYAML allowing to keep keys order."""
+
 import sys
+import yaml
 if float('%d.%d' % sys.version_info[:2]) < 2.7:
     from ordereddict import OrderedDict
 else:
     from collections import OrderedDict
 
 
+#
+## Loaders
+#
+def construct_yaml_map(self, node):
+    data = OrderedDict()
+    yield data
+    value = self.construct_mapping(node)
+    data.update(value)
+
+def construct_mapping(self, node, deep=False):
+    if isinstance(node, yaml.MappingNode):
+        self.flatten_mapping(node)
+    else:
+        msg = 'expected a mapping node, but found %s' % node.id
+        raise yaml.constructor.ConstructError(None,
+                                              None,
+                                              msg,
+                                              node.start_mark)
+
+    mapping = OrderedDict()
+    for key_node, value_node in node.value:
+        key = self.construct_object(key_node, deep=deep)
+        try:
+            hash(key)
+        except TypeError as err:
+            raise yaml.constructor.ConstructError(
+                'while constructing a mapping', node.start_mark,
+                'found unacceptable key (%s)' % err, key_node.start_mark)
+        value = self.construct_object(value_node, deep=deep)
+        mapping[key] = value
+    return mapping
+
 class Loader(yaml.Loader):
     def __init__(self, *args, **kwargs):
         yaml.Loader.__init__(self, *args, **kwargs)
+
         self.add_constructor(
             'tag:yaml.org,2002:map', type(self).construct_yaml_map)
         self.add_constructor(
             'tag:yaml.org,2002:omap', type(self).construct_yaml_map)
 
-    def construct_yaml_map(self, node):
-        data = OrderedDict()
-        yield data
-        value = self.construct_mapping(node)
-        data.update(value)
+    construct_yaml_map = construct_yaml_map
+    construct_mapping = construct_mapping
 
-    def construct_mapping(self, node, deep=False):
-        if isinstance(node, yaml.MappingNode):
-            self.flatten_mapping(node)
-        else:
-            msg = 'expected a mapping node, but found %s' % node.id
-            raise yaml.constructor.ConstructError(None,
-                                                  None,
-                                                  msg,
-                                                  node.start_mark)
+class SafeLoader(yaml.SafeLoader):
+    def __init__(self, *args, **kwargs):
+        yaml.SafeLoader.__init__(self, *args, **kwargs)
 
-        mapping = OrderedDict()
-        for key_node, value_node in node.value:
-            key = self.construct_object(key_node, deep=deep)
-            try:
-                hash(key)
-            except TypeError as err:
-                raise yaml.constructor.ConstructError(
-                    'while constructing a mapping', node.start_mark,
-                    'found unacceptable key (%s)' % err, key_node.start_mark)
-            value = self.construct_object(value_node, deep=deep)
-            mapping[key] = value
-        return mapping
+        self.add_constructor(
+            'tag:yaml.org,2002:map', type(self).construct_yaml_map)
+        self.add_constructor(
+            'tag:yaml.org,2002:omap', type(self).construct_yaml_map)
+
+    construct_yaml_map = construct_yaml_map
+    construct_mapping = construct_mapping
 
 
+#
+## Dumpers
+#
 def represent_ordereddict(self, data):
     return self.represent_mapping('tag:yaml.org,2002:map', data.items())
 
